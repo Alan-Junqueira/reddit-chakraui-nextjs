099870cfbc9bb4cbd96147c23aac317e8eb8bba8
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-02-23 09:29:36 -0300

    style: Community directory in NavBar

---
+++ src/components/partials/NavBar/Directory/index.tsx
@@ -0,0 +1,42 @@
+import { authModalState } from '@/atoms/authModalAtom';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react';
+import { useSetRecoilState } from 'recoil';
+import { TiHome } from 'react-icons/ti';
+
+export const Directory = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+
+  return (
+    <Menu>
+      <MenuButton
+        cursor="pointer"
+        padding="0 6px"
+        borderRadius={4}
+        mr={2}
+        ml={{ base: 0, md: 2 }}
+        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
+      >
+        <Flex
+          align="center"
+          justify="space-between"
+          width={{ base: 'auto', lg: '200px' }}
+        >
+          <Flex align="center">
+            <Icon as={TiHome} fontSize={24} mr={{ base: 1, md: 2 }} />
+            <Flex display={{ base: 'none', lg: 'flex' }}>
+              <Text fontWeight={600} fontSize="10pt">
+                Home
+              </Text>
+            </Flex>
+          </Flex>
+          <ChevronDownIcon />
+        </Flex>
+      </MenuButton>
+      <MenuList>
+        {/* <Comunities /> */}
+        Comunities
+      </MenuList>
+    </Menu>
+  );
+};
--- src/components/partials/NavBar/index.tsx
+++ src/components/partials/NavBar/index.tsx
@@ -2,21 +2,36 @@ import { auth } from '@/services/firebase/clientApp';
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Directory } from './Directory';
 import { RightContent } from './RightContent';
 import { SearchInput } from './SearchInput';
 
 export const NavBar = () => {
-  const [user, loading, error] = useAuthState(auth)
+  const [user, loading, error] = useAuthState(auth);
 
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
-        <Image src="/images/redditFace.svg" alt="logo reddit"  height="30px"/>
-        <Image src="/images/redditText.svg" alt="reddit" height="46px" display={{base: 'none', md: 'unset'}}/>
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: 'space-between' }}
+    >
+      <Flex
+        align="center"
+        width={{ base: '40px', md: 'auto' }}
+        mr={{ base: 0, md: 2 }}
+      >
+        <Image src="/images/redditFace.svg" alt="logo reddit" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="reddit"
+          height="46px"
+          display={{ base: 'none', md: 'unset' }}
+        />
       </Flex>
-      {/* <Directory /> */}
-      <SearchInput />
-      <RightContent user={user}/>
+      {user && <Directory />}
+      <SearchInput user={user} />
+      <RightContent user={user} />
     </Flex>
   );
 };
